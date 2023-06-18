d770c69a81f18979c7b768e7b034ff5531a2bad7
qusai122 <qusai.2010.123@gmail.com>
2023-06-18 19:22:18 +0300

    done

--- todos-project/src/controllers/todos.ts
+++ todos-project/src/controllers/todos.ts
@@ -10,34 +10,44 @@ export const createToDo: RequestHandler = async (req, res, next) => {
 };
 
 export const getToDos: RequestHandler = async (req, res, next) => {
-  const todos: Todos[]  = await Todos.findAll();
+  const todos: Todos[] = await Todos.findAll();
   return res
     .status(200)
     .json(todos);
 };
 
-export const updateToDos: RequestHandler = async (req, res, next) => {
-const {id} = req.params;
-  const result: any[]  = await Todos.update({ ...req.body },{where:{id}});
-  if(result[0]){
-    return res
+async function getElementById(id: number) {
+  return await Todos.findByPk(id);
+}
+
+export const getOneToDo: RequestHandler = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await getElementById(parseInt(id))
+  return res
     .status(200)
-    .json({ ...req.body })
+    .json(result);
+};
+
+export const updateToDos: RequestHandler = async (req, res, next) => {
+  const { id } = req.params;
+  const result: any[] = await Todos.update({ ...req.body }, { where: { id } });
+  if (result[0]) {
+    return res
+      .status(200)
+      .json({ ...req.body })
   }
   return res
-  .status(400)
-  .json("the file wan't updated")
+    .status(400)
+    .json("the file wan't updated")
 };
 
 export const deleteToDos: RequestHandler = async (req, res, next) => {
-  const {id } = req.params;
-    const result = await Todos.destroy({where:{id}});
-    if(result){
-      return res
-      .status(200)
-      .json("it was deleted")
-    }
-    return res
-    .status(400)
-    .json("the file wan't deleted")
-  };
+  const { id } = req.params;
+  const todo = await getElementById(parseInt(id));
+
+  if (!todo)
+    return res.status(404).json({ message: "there is no todo with this id" });
+  const affectedRows = await Todos.destroy({ where: { id: id } });
+  res.status(200).json(affectedRows);
+
+};
--- todos-project/src/routes/todos.ts
+++ todos-project/src/routes/todos.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import {
-  createToDo, deleteToDos, getToDos, updateToDos,getElementByid,
+  createToDo, deleteToDos, getToDos, updateToDos,getOneToDo,
 } from "../controllers/todos";
 const router = Router();
 router.post("/", createToDo);
 router.get("/", getToDos);
-router.get("/:id", getElementByid);
+router.get("/:id", getOneToDo);
 router.put("/:id", updateToDos);
 router.delete("/:id", deleteToDos);
 export default router;
